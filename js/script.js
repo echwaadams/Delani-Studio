9c5db09d5d6e41594de5e80f146efd8ae6f93a45
echwaadams <adamsechwa18@gmail.com>
2021-09-10 14:35:10 +0300

    icons toggle between respective description of each icon when clicked, centered

---
+++ js/script.js
@@ -0,0 +1,29 @@
+$(document).ready(function(){
+     // hiding the paragraphs
+     $('#p1').hide();
+     $('#p2').hide();
+     $('#p3').hide();
+ 
+ 
+     // toggling between the paragraphs and icons
+     $('#img1').click(function(){
+         $('#p1').toggle('slow')
+         $('#img1').slideUp();
+     });
+ 
+     $('#p1').click(function() {
+         $('#p1').hide()
+         $('img1').show()
+     });
+ 
+     $('#img2').click(function(){
+         $('#p2').toggle('slow')
+         $('#img2').slideUp();
+     });
+ 
+ 
+     $('#img3').click(function(){
+         $('#p3').toggle('slow')
+         $('#img3').slideUp();
+     });
+});
