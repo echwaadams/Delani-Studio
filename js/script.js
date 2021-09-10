18d69d95ac1405ab1d7fe1d15bf53d46e4db03b4
echwaadams <adamsechwa18@gmail.com>
2021-09-10 18:04:41 +0300

    minor update

--- js/script.js
+++ js/script.js
@@ -33,14 +33,14 @@ $(document).ready(function(){
     });
 
     // form validation
-    $('form#contacts').submit(function(event) {
+    $('form#contact').submit(function(event) {
         const name = $('input#name').val();
         const email = $('input#email').val();
         const message = $('textarea#message').val();
 
         // popping a message to alert the user for filling the form
         if($("input#name").val() && $("input#email").val()) {
-            alert(' Hi ' + name + 'We have received your message. Thank you for reaching out to us');
+            alert(' Hi ' + name + ' We have received your message. Thank you for reaching out to us');
         }
         else {
             alert('Please fill in the form');
